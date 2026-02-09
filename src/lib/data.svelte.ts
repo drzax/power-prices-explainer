2ea82e01006f0078128f5bba42d8d227fc3e24bf
Simon Elvery <simon@elvery.net>
2026-02-09 16:55:15 +1000

    Add a todo about data

--- src/lib/data.svelte.ts
+++ src/lib/data.svelte.ts
@@ -3,3 +3,5 @@ import series from '../data/data-c0JWJ.csv';
 import { DataSchema } from './schemas';
 
 export const data = parse(DataSchema, series).map(d => ({ ...d, incl: d.incl || d.excl }));
+
+// TODO: handle generic data. Possibly with type inference https://observablehq.com/@d3/d3-autotype
